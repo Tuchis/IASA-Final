518ec1afaba25594798bb9448924fc7956d7869f
Tuchis <humennyy.vlad@gmail.com>
2024-02-11 00:06:18 +0200

    Darkest commit before dawn

--- gpt4/gpt4v.py
+++ gpt4/gpt4v.py
@@ -9,40 +9,6 @@ def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Replace 'path_to_your_image.jpg' with the path to your actual image file
-# image_path = "/home/kuzhum/IASA/IASA-Final-Vlad/test-suite/ClearVPN-1707081884.62.png"
-# base64_image = encode_image(image_path)
-
-# headers = {
-#     "Content-Type": "application/json",
-#     "Authorization": f"Bearer {api_key}"
-# }
-
-# prompt = "You have a photo of UI interface of an app. Describe every element of UI. Include all buttons and text fields. Do not talk about color, style etc"
-
-# payload = {
-#     "model": "gpt-4-vision-preview",
-#     "messages": [
-#       {
-#         "role": "user",
-#         "content": [
-#           {"type": "text", "text": prompt},
-#           {
-#             "type": "image_url",
-#             "image_url": {
-#               "url": f"data:image/jpeg;base64,{base64_image}"
-#             }
-#           }
-#         ]
-#       }
-#     ],
-#     "max_tokens": 500
-# }
-
-# # Make the API request and print out the response
-# response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-# print(response.json())
-
 def gpt4v_response(image_path, prompt, max_tokens=500):
     base64_image = encode_image(image_path)
 
--- gpt4/runner.py
+++ gpt4/runner.py
@@ -2,42 +2,45 @@ import os
 from gpt4v import gpt4v_response
 
 file_folder = "/home/kuzhum/IASA/IASA-Final-Vlad/IASA_Champ_Final/app_data_splitted/test"
-result_folder = "/home/kuzhum/IASA/IASA-Final-Vlad/gpt4_results"
+result_folder = "/home/kuzhum/IASA/IASA-Final-Vlad/gpt4_test"
 
 prompt = "You have a photo of UI interface of an app. Describe every element of UI. Include all buttons and text fields. Do not talk about color, style etc"
-max_tokens = 500
+max_tokens = 800
 
-response = gpt4v_response('/home/kuzhum/IASA/IASA-Final-Vlad/IASA_Champ_Final/app_data_splitted/test/KeyKey Typing Tutor/1707238869/-1707238871.87.png', prompt, max_tokens)
+FINAL_RUN = True
 
-print(response)
+# response = gpt4v_response('/home/kuzhum/IASA/IASA-Final-Vlad/IASA_Champ_Final/app_data_splitted/test/GlueMotion/1707240051/GlueMotion-1707240052.37.png', prompt, max_tokens)
 
-content = response['choices'][0]['message']['content']
+# print(response)
 
-# Save content to .txt file in result_folder
-with open(os.path.join(result_folder, "KeyKey Typing Tutor-1707238869.txt"), 'w') as file:
-    file.write(content)
+# content = response['choices'][0]['message']['content']
 
-# counter = 0
+# # Save content to .txt file in result_folder
+# with open(os.path.join(result_folder, "GlueMotion-1707240051.extension"), 'w') as file:
+#     file.write(content)
 
-for folder in os.listdir(file_folder):
-    if folder.endswith('DS_Store'):
-        continue
-    for screenshot in os.listdir(os.path.join(file_folder, folder)):
-        if screenshot.endswith('DS_Store'):
+counter = 0
+
+if FINAL_RUN:
+    for folder in os.listdir(file_folder):
+        if folder.endswith('DS_Store'):
             continue
-        for file in os.listdir(os.path.join(file_folder, folder, screenshot)):
-            if file.endswith('DS_Store'):
+        for screenshot in os.listdir(os.path.join(file_folder, folder)):
+            if screenshot.endswith('DS_Store'):
                 continue
-            if file.endswith('.json') or file.endswith('box.png'):
-                continue
-            else:
-                print(folder, screenshot, file)
-                counter += 1
-                response = gpt4v_response(os.path.join(file_folder, folder, screenshot, file), prompt, max_tokens)
-                content = response['choices'][0]['message']['content']
-                with open(os.path.join(result_folder, f"{folder}_{screenshot}.extension"), 'w') as file:
-                    file.write(content)
+            for file in os.listdir(os.path.join(file_folder, folder, screenshot)):
+                if file.endswith('DS_Store'):
+                    continue
+                if file.endswith('.json') or file.endswith('box.png'):
+                    continue
+                else:
+                    print(folder, screenshot, file)
+                    counter += 1
+                    response = gpt4v_response(os.path.join(file_folder, folder, screenshot, file), prompt, max_tokens)
+                    content = response['choices'][0]['message']['content']
+                    with open(os.path.join(result_folder, f"{folder}_{screenshot}.extension"), 'w') as file:
+                        file.write(content)
 
-    print("\n")
+        print("\n")
 
 print(counter)
